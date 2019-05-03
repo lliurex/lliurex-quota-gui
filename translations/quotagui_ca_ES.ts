b6de3e810d456517027e66f09572c8c2c14213bb
M.Angel Juan <m.angel.juan@gmail.com>
2019-05-03 14:41:21 +0200

    Apply qtranslation system

---
+++ translations/quotagui_ca_ES.ts
@@ -0,0 +1,222 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca_ES">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>Lliurex-quota editor</source>
+        <translation>Lliurex-quota editor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="156"/>
+        <source>User:</source>
+        <translation>Usuari:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="173"/>
+        <source>Password:</source>
+        <translation>Contrasenya:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="246"/>
+        <source>Login</source>
+        <translation>Valida</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <location filename="mainwindow.ui" line="398"/>
+        <source>Filter:</source>
+        <translation>Filtre:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="322"/>
+        <source>Show all groups</source>
+        <translation>Mostra tots els grups</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="346"/>
+        <location filename="mainwindow.ui" line="445"/>
+        <source>Undo changes</source>
+        <translation>Desfes tots els canvis</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="353"/>
+        <location filename="mainwindow.ui" line="452"/>
+        <location filename="mainwindow.ui" line="558"/>
+        <source>Apply</source>
+        <translation>Aplica</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="373"/>
+        <source>This system need to be configured before you can edit something</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="504"/>
+        <source>Pending actions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="551"/>
+        <source>Back</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="581"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="611"/>
+        <location filename="mainwindow.ui" line="614"/>
+        <source>Enable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="626"/>
+        <location filename="mainwindow.ui" line="629"/>
+        <source>Disable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="638"/>
+        <source>UserEditor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="641"/>
+        <location filename="mainwindow.ui" line="653"/>
+        <source>Switch to group editor mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="650"/>
+        <source>GroupEditor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="662"/>
+        <source>Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="665"/>
+        <source>Refresh views</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="655"/>
+        <source>Used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="656"/>
+        <source>QuotaApplied</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="600"/>
+        <location filename="mainwindow.cpp" line="621"/>
+        <location filename="mainwindow.cpp" line="1049"/>
+        <location filename="mainwindow.cpp" line="1052"/>
+        <location filename="mainwindow.cpp" line="1070"/>
+        <source>Quota</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="206"/>
+        <source>Refreshing information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="376"/>
+        <source>Wait 5 seconds while syncing changes...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="505"/>
+        <source>Ready</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="557"/>
+        <source>Checking validation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="560"/>
+        <source>Validation successful</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="563"/>
+        <source>Validation failed: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="573"/>
+        <source>Refreshed information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="575"/>
+        <source>System is currently configured</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="580"/>
+        <source>System is currently unconfigured: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="591"/>
+        <location filename="mainwindow.cpp" line="648"/>
+        <location filename="mainwindow.cpp" line="688"/>
+        <source>Something goes wrong: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="706"/>
+        <source>System quotas are enabled now</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="709"/>
+        <location filename="mainwindow.cpp" line="721"/>
+        <source>Something goes wrong</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="718"/>
+        <source>System quotas are disabled now</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="730"/>
+        <source>Failed to set user quota</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="743"/>
+        <source>Failed to set group quota</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1071"/>
+        <source>Previous value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1215"/>
+        <source>Invalid value</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/quotagui_es_ES.ts
@@ -0,0 +1,222 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>Lliurex-quota editor</source>
+        <translation>Lliurex-quota editor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="156"/>
+        <source>User:</source>
+        <translation>Usuario:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="173"/>
+        <source>Password:</source>
+        <translation>Contraseña:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="246"/>
+        <source>Login</source>
+        <translation>Entrar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <location filename="mainwindow.ui" line="398"/>
+        <source>Filter:</source>
+        <translation>Filtro:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="322"/>
+        <source>Show all groups</source>
+        <translation>Mostrar todos los grupos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="346"/>
+        <location filename="mainwindow.ui" line="445"/>
+        <source>Undo changes</source>
+        <translation>Deshacer los cambios</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="353"/>
+        <location filename="mainwindow.ui" line="452"/>
+        <location filename="mainwindow.ui" line="558"/>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="373"/>
+        <source>This system need to be configured before you can edit something</source>
+        <translation>El sistema tiene que configurarse para poder editar las cuotas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="504"/>
+        <source>Pending actions</source>
+        <translation>Acciones pendientes</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="551"/>
+        <source>Back</source>
+        <translation>Atrás</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="581"/>
+        <source>toolBar</source>
+        <translation>Barra de herramientas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="611"/>
+        <location filename="mainwindow.ui" line="614"/>
+        <source>Enable</source>
+        <translation>Activar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="626"/>
+        <location filename="mainwindow.ui" line="629"/>
+        <source>Disable</source>
+        <translation>Desactivar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="638"/>
+        <source>UserEditor</source>
+        <translation>Cuotas de usuario</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="641"/>
+        <location filename="mainwindow.ui" line="653"/>
+        <source>Switch to group editor mode</source>
+        <translation>Cambiar a edición de cuotas de grupo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="650"/>
+        <source>GroupEditor</source>
+        <translation>Cuotas de grupos</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="662"/>
+        <source>Refresh</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="665"/>
+        <source>Refresh views</source>
+        <translation>Actualizar vistas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="654"/>
+        <source>Used</source>
+        <translation>Espacio utilizado</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="655"/>
+        <source>QuotaApplied</source>
+        <translation>Cuota aplicada</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <location filename="mainwindow.cpp" line="599"/>
+        <location filename="mainwindow.cpp" line="620"/>
+        <location filename="mainwindow.cpp" line="1048"/>
+        <location filename="mainwindow.cpp" line="1051"/>
+        <location filename="mainwindow.cpp" line="1069"/>
+        <source>Quota</source>
+        <translation>Cuota</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="206"/>
+        <source>Refreshing information</source>
+        <translation>Actualizando información</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="375"/>
+        <source>Wait 5 seconds while syncing changes...</source>
+        <translation>Esperando 5 segundos mientras se aplican los cambios</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="504"/>
+        <source>Ready</source>
+        <translation>Preparado</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="556"/>
+        <source>Checking validation</source>
+        <translation>Comprobando la validación</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="559"/>
+        <source>Validation successful</source>
+        <translation>Validación correcta</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="562"/>
+        <source>Validation failed: </source>
+        <translation>Validación fallida</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="572"/>
+        <source>Refreshed information</source>
+        <translation>Información actualizada</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="574"/>
+        <source>System is currently configured</source>
+        <translation>El sistema está actualmente activado</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="579"/>
+        <source>System is currently unconfigured: </source>
+        <translation>El sistema está actualmente desactivado</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="590"/>
+        <location filename="mainwindow.cpp" line="647"/>
+        <location filename="mainwindow.cpp" line="687"/>
+        <source>Something goes wrong: </source>
+        <translation>Algo ha ido mal:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="705"/>
+        <source>System quotas are enabled now</source>
+        <translation>El sistema de cuotas está ahora activado</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="708"/>
+        <location filename="mainwindow.cpp" line="720"/>
+        <source>Something goes wrong</source>
+        <translation>Algo ha ido mal</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="717"/>
+        <source>System quotas are disabled now</source>
+        <translation>El sistema de cuotas está ahora desactivado</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="729"/>
+        <source>Failed to set user quota</source>
+        <translation>Fallo al intentar fijar la cuota de usuario</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="742"/>
+        <source>Failed to set group quota</source>
+        <translation>Fallo al intentar fijar la cuota de grupo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1070"/>
+        <source>Previous value</source>
+        <translation>Valor anterior</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="1214"/>
+        <source>Invalid value</source>
+        <translation>Valor invalido</translation>
+    </message>
+</context>
+</TS>
